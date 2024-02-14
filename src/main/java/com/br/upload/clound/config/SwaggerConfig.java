afa61c6f21007b5ce5a97a222b8b90ddfa2f5856
Evandrolds <evandro.lds.ds@outlook.com>
2024-02-14 14:26:08 -0300

    Configurações do Swagger e HttpCleint criada

---
+++ src/main/java/com/br/upload/clound/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.br.upload.clound.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.br.config")) // Pacote onde estão os controllers
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API Documentation")
+                .description("Documentação da API")
+                .version("1.0.0")
+                .build();
+    }
+}
+
