498c94d03a19ef527122379e6b87d7c6011f56ad
Evandrolds <evandro.lds.ds@outlook.com>
2024-02-14 14:24:42 -0300

    Projeto criado

---
+++ src/main/java/com/br/upload/clound/CloundApplication.java
@@ -0,0 +1,13 @@
+package com.br.upload.clound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloundApplication.class, args);
+	}
+
+}
